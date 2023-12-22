598a51d0168f536f31ed2f59477c3f31213b6f32
iamshaylajade <shaylajade21@yahoo.com>
2023-12-21 23:56:46 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cfonts = require('cfonts');
+
+connection.connect((err) => {
+    if (err) throw err;
+
+    //Creating function for employees
+    function viewEmployees() {
+        
+        connection.query(query, (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            // restart the application
+            start();
+        });
+    }
